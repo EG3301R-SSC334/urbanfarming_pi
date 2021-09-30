bb95173608b8dc6b3a9ebbb61c0145bee81e1666
huachen <huachenw24@gmail.com>
2021-09-30 13:18:37 +0800

    finished control system less peristaltic pumps

--- urbanfarming_pi.py
+++ urbanfarming_pi.py
@@ -15,6 +15,7 @@ f = open('id.json')
 id = json.load(f)
 data = {}
 resched_flag = 1
+pump_flag = 0
 
 def firstPost(systemName, ownerID, plantType):
     global id
@@ -48,21 +49,18 @@ def post():
 
 def readData():
     if ser.in_waiting > 0:
+        # line = ser.readline().rstrip()
+        # print(line)
         line = ser.readline().rstrip().decode()
         data['EC'] = unpack('<f', bytes.fromhex(line[0:8]))[0]
         data['pH'] = unpack('<f', bytes.fromhex(line[8:16]))[0]
         data['temp'] = unpack('<f', bytes.fromhex(line[16:24]))[0]
         data['humidity'] = unpack('<f', bytes.fromhex(line[24:32]))[0]
-        print("reading data")
+        print("received sensor data")
 
-def mainPumpOn():
-    print("pump ON")
-    ser.write(b"11\n")
-
-def mainPumpOff():
-    print("pump OFF")
-    ser.write(b"10\n")
-
+### mainpump(1), onduration(6), offduration(6)
+### peristaltic pump(1), pumpselect(1)
+### light(1), on/off(1)
 def pPump():
     print("peristaltic")
     ser.write(b"21\n")
@@ -78,19 +76,38 @@ def lightOff():
 def background_schedule():
     global scheduler
     scheduler = BackgroundScheduler()
-    scheduler.add_job(mainPumpOn, 'interval', seconds=5, id="pumpon") #minutes=17)
-    scheduler.add_job(mainPumpOff, 'interval', seconds=7, id="pumpoff") #minutes=17)
-    scheduler.add_job(lightOn, 'cron', minute='*/1', id="lighton") #hour=9)
-    scheduler.add_job(lightOff, 'cron', minute='*/2', id="lightoff") #hour=18)
+    scheduler.add_job(lightOn, 'cron', second=0, id="lighton") #hour=9)
+    scheduler.add_job(lightOff, 'cron', second=30, id="lightoff") #hour=18)
     scheduler.add_job(post, 'cron', minute='*/10')
     scheduler.start()
 
+# need to get data from server?
+def changeLightHours(on, off):
+    global scheduler
+    print("changing light hours")
+    scheduler.reschedule_job("lighton", trigger='cron', hour=on)
+    scheduler.reschedule_job("lightoff", trigger='cron', hour=off)
+
+def changePumpInterval(on, off):
+    print("changing pump interval")
+    onstr = ("0" * (6 - len(str(on)))) + str(on)
+    offstr = ("0" * (6 - len(str(off)))) + str(off)
+    data = "1" + onstr + offstr + "\n"
+    ser.write(bytes(data, 'UTF-8'))
+
 def controlSystem():
-    global data, scheduler, resched_flag
-    if resched_flag:
-        print("rescheduling")
-        scheduler.reschedule_job("pumpon", trigger='interval', seconds=20)
-        resched_flag = 0
+    global data
+    # test humidity
+    # if data['humidity'] < 13262:
+    #   changePumpInterval(123123,1241241)
+    # else if data['humidity'] > 12353:
+    #   changePumpInterval(123123,1241241)
+    #
+    # test ec
+    # if data['EC'] < 123123:
+    #   asdasd
+    # else if data['EC'] > 23412:
+    #   asdad
 
 if __name__ == '__main__':
     # firstPost("testsystem", 1, "testplant", {"humidity": "80", "temp": "25", "pH":"6.5", "EC":"1.6"})
